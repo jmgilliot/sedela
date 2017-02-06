a1a8daf8c811c5cb6a486d6b67fa8c9131429449
System Administrator <root@MacBook-Pro.local>
2017-02-06 15:36:08 +0100

    update
    modified:   controllers/ProfileController.js
    	modified:   package.json
    	modified:   partials/profile.html

--- controllers/ProfileController.js
+++ controllers/ProfileController.js
@@ -1,35 +1,24 @@
-angular.module('portfolioapp').controller('ProfileController', ProfileController);
+'use strict';
+
+
+angular.module('portfolioapp')
+.controller('ProfileController', ProfileController);
 
 ProfileController.$inject = ['CozySdk'];
 
 function ProfileController(CozySdk) {
+      var vm = this;
 
-	var vm = this;
-    vm.send = send;
-    vm.update = update;
+    function insertProfile() {
 
+var attributes = {title:"hello", content:"world"}
+CozySdk.create('Note', attributes, function(err, obj){
+    console.log(obj.id)
+});
 
-function send(user) {
-        CozySdk.create('Profile', user)
-        .then(resetForm)
-        .then(updateContactList)
-        .catch(function(error) {
-          vm.error = error;
-        });
+    
     }
 
- function updateProfile()) {
-        CozySdk.defineRequest('Profile', 'all', 'function(doc) { emit(doc.n); }')
-        .then(function () {
-          return CozySdk.runRequest('Profile', 'all')
-        })
-        .then(function(res) {
-          vm.profile = res;
-        })
-        .catch(function(error) {
-            vm.error = error;
-        });
-      }
+  }
 
 
-}
