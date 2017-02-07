3879bf2f2a9539845d7503aa7aaf9fdd7f0b53f3
Nicolastanguy <nicolas_tanguy@icloud.com>
2017-02-07 19:35:20 +0100

    update

--- controllers/editProfileController.js
+++ controllers/editProfileController.js
@@ -2,13 +2,48 @@
 
 
 angular.module('portfolioapp')
-.controller('ProfileController', ProfileController);
+.controller('EditProfileController', EditProfileController);
 
-ProfileController.$inject = ['CozySdk'];
+EditProfileController.$inject = ['CozySdk'];
 
-function ProfileController(CozySdk) {
+function EditProfileController(CozySdk) {
       var vm = this;
       vm.insertProfile = insertProfile;
+      vm.profiles = {};
+
+      updateProfileList();
+
+
+      function updateProfileList() {
+
+        CozySdk.defineRequest('Profile', 'all', 'function(doc) {emit(doc.FirstName, {FirstName: doc.firstName, Address: doc.address,description:doc.description});}')
+        .then(function () {
+          return CozySdk.runRequest('Profile', 'all')
+        })
+        .then(function(res) {
+          vm.profiles = res;
+          console.log(JSON.stringify(vm.profiles));
+          console.log(JSON.stringify(vm.profiles[1].value.FirstName));
+
+         // console.log(JSON.stringify(vm.profile));
+        })
+        .catch(function(error) {
+            vm.error = error;
+        });
+      }
+
+      // function update(id, user) {
+      //   var profileName = {
+      //     firstName: user.firstName,
+      //     lastName: user.lastName
+      //   };
+
+      //   CozySdk.update('Profile', id, profile)
+      //   .then(updateProfileList)
+      //   .catch(function(error) {
+      //       vm.error = error;
+      //   });
+      // }
 
 
         function resetForm() {
@@ -16,15 +51,13 @@ function ProfileController(CozySdk) {
         vm.profile = angular.copy(defaultForm);
       }
    
-    function insertProfile(user) {
-        CozySdk.create('Profile', user)
-        .then(resetForm)
+    function insertProfile(profile) {
+        CozySdk.create('Profile', profile)
+        .then(updateProfileList)
         .catch(function(error) {
           vm.error = error;
         });
     }
-
-    
 
   }
 
