f4d7e06b27faf3bbeea552c23af79d322b515615
Nicolastanguy <nicolas_tanguy@icloud.com>
2017-02-08 11:42:38 +0100

    update profile successful

--- controllers/EditProfileController.js
+++ controllers/EditProfileController.js
@@ -9,54 +9,53 @@ EditProfileController.$inject = ['CozySdk'];
 function EditProfileController(CozySdk) {
       var vm = this;
       vm.insertProfile = insertProfile;
+      vm.updateProfile=updateProfile;
       vm.profile ={};
       updateProfileList();
 
       function updateProfileList() {
 
-        CozySdk.defineRequest('Profile', 'all', 'function(doc) {emit(doc.FirstName, {FirstName: doc.firstName, Address: doc.address,description:doc.description});}')
+        CozySdk.defineRequest('Profile', 'all', 'function(doc) {emit(doc.firstName, {firstName: doc.firstName, lastName: doc.lastName,dateOfBirth: doc.dateOfBirth,email:doc.email,phone:doc.phone,hobbies:doc.hobbies,keywords:doc.keywords,diploma:doc.diploma,occupation:doc.occupation,skills:doc.skills,address: doc.address,description:doc.description});}')
         .then(function () {
           return CozySdk.runRequest('Profile', 'all')
         })
         .then(function(res) {
           vm.profiles = res;
-          vm.profile = vm.profiles[1].value;
-          console.log(JSON.stringify(vm.profiles));
-          console.log(JSON.stringify(vm.profile));
-
-         // console.log(JSON.stringify(vm.profile));  
+          vm.profile = vm.profiles[0];
+         // console.log('LOG PROFILES : ',JSON.stringify(vm.profiles));
+         //console.log(JSON.stringify(vm.profiles.length));  
         })
         .catch(function(error) {
             vm.error = error;
         });
       }
 
-      // function update(id, user) {
-      //   var profileName = {
-      //     firstName: user.firstName,
-      //     lastName: user.lastName
-      //   };
-
-      //   CozySdk.update('Profile', id, profile)
-      //   .then(updateProfileList)
-      //   .catch(function(error) {
-      //       vm.error = error;
-      //   });
-      // }
-
-
-        function resetForm() {
-        var defaultForm = {};
-        vm.profile = angular.copy(defaultForm);
+    function updateProfile(id, profile) { 
+      if (vm.profiles.length==0) {
+        insertProfile(vm.profile.value)
       }
+      else {
+         var profileUpdated = angular.copy(vm.profile.value);
+         //console.log('////////////////',JSON.stringify(profileUpdated));  
+         CozySdk.update('Profile', id, profileUpdated)
+         .then(updateProfileList)
+         .catch(function(error) {
+             vm.error = error;
+         });
+      }
+      }
+      //   function resetForm() {
+      //   var defaultForm = {};
+      //   vm.profile = angular.copy(defaultForm);
+      // }
    
-    function insertProfile(profile) {
-        CozySdk.create('Profile', profile)
-        .then(resetForm)
-        .then(updateProfileList)
+     function insertProfile(profile) {
+         CozySdk.create('Profile', profile)
+        //.then(resetForm)
+         .then(updateProfileList)
         .catch(function(error) {
           vm.error = error;
-        });
+         });
     }
 
   }
